69d5265baf24b18104901fb09bae311045eac1e2
Lingeshwaransenthilkumar <lingeshwaransenthilkumar0279@gmail.com>
2024-09-02 13:46:57 +0530

    third commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
 import reportWebVitals from './reportWebVitals';
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter basename="/school-website/">
     <React.StrictMode>
       <App />
     </React.StrictMode>,
-  </BrowserRouter>
 )
 
 reportWebVitals();
